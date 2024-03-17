6a8a184c553590859a1dfbffe09d25a57d87dd26
Christoph Müller <mail@christoph.mx>
2024-03-17 14:49:22 +0100

    feat: add wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,20 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = "Solarized Dark Higher Contrast"
+
+-- Change window opacity
+config.window_background_opacity = 0.9
+
+-- Change font
+config.font = wezterm.font("Fira Code Nerd Font")
+config.font_size = 16
+
+-- and finally, return the configuration to wezterm
+return config
