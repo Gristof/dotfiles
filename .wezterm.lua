bae4cc4f1e15cb224cc06989bc27e54f7aa72654
Christoph Müller <mail@christoph.mx>
2024-03-17 15:31:27 +0100

    change wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = "Solarized Dark Higher Contrast"
+config.color_scheme = "Rosé Pine (Gogh)"
 
 -- Change window opacity
 config.window_background_opacity = 0.9
