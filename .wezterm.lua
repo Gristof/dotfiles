a32c06767c1a4ec9fe1c4c74b6a99b943cb1aed8
Christoph Müller <mail@christoph.mx>
2024-06-10 11:37:51 +0200

    make changes to wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,18 +7,25 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = "Rosé Pine (Gogh)"
+config.color_scheme = "Gruvbox Dark (Gogh)"
 
 -- Change window opacity
 config.window_background_opacity = 0.9
 
 -- Change font
-config.font = wezterm.font("Fira Code Nerd Font")
-config.font_size = 16
+config.font = wezterm.font("UbuntuMono Nerd Font")
+config.font_size = 18
+config.line_height = 1.2
+-- Antialiasing
+config.freetype_load_target = "Light"
 
 -- Tab adjustments
-use_fancy_tab_bar = false
-tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+config.hide_tab_bar_if_only_one_tab = true
+
+-- Window adjustments
+config.window_decorations = "NONE"
 
 -- and finally, return the configuration to wezterm
 return config
