21af31e5f98ee622a33cdfc0c4a2a7da77273722
Christoph Müller <mail@christoph.mx>
2025-01-03 19:44:21 +0100

    change terminal configs

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,17 +7,25 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = "Kanagawa (Gogh)"
+config.color_scheme = "terafox"
 
 -- Change window opacity
 config.window_background_opacity = 0.9
 
 -- Change font
-config.font = wezterm.font("UbuntuSansMono Nerd Font Mono")
-config.font_size = 14
-config.line_height = 1.2
+config.font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "Medium" })
+config.font_size = 13
+-- config.line_height = 1.2
 -- Antialiasing
 -- config.freetype_load_target = "Light"
+
+-- Window padding
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
 
 -- Tab adjustments
 config.use_fancy_tab_bar = false
